0d706c30c54c2e3280bcd3d89a0ebe7b371167e4
Gustav-Br <99103909+Gustav-Br@users.noreply.github.com>
2024-02-13 01:12:39 -0300

    arreglando grid imagenes, y responsive

--- imagen.js
+++ imagen.js
@@ -1,12 +1,35 @@
+var currentIndex = 0;
+
 function mostrarImagen(src) {
     var imagenGrandeContenedor = document.getElementById("imagen-grande-contenedor");
     var imagenGrande = document.getElementById("imagen-grande");
 
     imagenGrande.src = src;
     imagenGrandeContenedor.style.display = "block";
+    
+    var imagenes = document.querySelectorAll('.imagen');
+    currentIndex = Array.from(imagenes).findIndex(img => img.src === src);
+
 }
 
 function ocultarImagen() {
     var imagenGrandeContenedor = document.getElementById("imagen-grande-contenedor");
-    imagenGrandeContenedor.style.display = "none";
+    if (event.target.className === "cerrar-imagen") {
+        imagenGrandeContenedor.style.display = "none";
+    }
+}
+
+document.addEventListener("click", ocultarImagen);
+
+
+function mostrarSiguienteImagen() {
+    var imagenes = document.querySelectorAll('.imagen');
+    currentIndex = (currentIndex + 1) % imagenes.length;
+    mostrarImagen(imagenes[currentIndex].src);
+}
+
+function mostrarImagenAnterior() {
+    var imagenes = document.querySelectorAll('.imagen');
+    currentIndex = (currentIndex - 1 + imagenes.length) % imagenes.length;
+    mostrarImagen(imagenes[currentIndex].src);
 }
